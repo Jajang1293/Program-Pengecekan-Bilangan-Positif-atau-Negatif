db6f2a2162fb697d7b49089c3811004743556790
Jajang Jaelani <45264588+Jajang1293@users.noreply.github.com>
2018-12-31 05:35:03 +0700

    Add files via upload

---
+++ Program Periksa Bilangan Positif atau Negatif.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int x;
+    printf("Masukan Bilangan : ");scanf("%d",&x);
+    if(x>=0)
+    {
+        printf("%d adalah bilangan positif\n",x);
+    }else{printf("%d adalah bilangan negatif\n",x);
+    }
+}
